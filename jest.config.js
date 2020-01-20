9a8ddcacc1555d02bb678c794b6fd1d598c5fae7
Takuho Yoshizu <yoshizu@s2factory.co.jp>
2020-01-20 19:31:32 +0900

    test

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  },
+  testMatch: ['**/*.spec.+(ts|tsx)'],
+  moduleNameMapper: {
+    '~/(.*)': '<rootDir>/$1'
+  },
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/'
+  ]
+}
